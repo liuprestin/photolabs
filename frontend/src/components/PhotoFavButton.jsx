91ae64a16e62a463cb9b4f5ab39b3396ba9b85b0
Prestin Liu <liuprestin@proton.me>
2023-09-12 22:53:52 -0700

    add useState, add onClick() handler

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,17 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+  const [selected, setSelected] = useState(false);
+
+  const handleClick = () => {
+    console.log("button cilcked:", selected);
+    setSelected(!selected)
+  };
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={selected} onClick={handleClick}/>
       </div>
     </div>
   );
