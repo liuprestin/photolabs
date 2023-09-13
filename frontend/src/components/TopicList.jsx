a6c3e008734d481b76b9c7a1520019557ccd0d4c
Prestin Liu <liuprestin@proton.me>
2023-09-12 22:56:24 -0700

    component to merge each TopicListItem , props pass down information as intended

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,20 @@
 import React from "react";
 
+import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const TopicList = () => {
+const TopicList = (props) => {
+  const topics = props.topicListData.map(topic => {
+    return (
+    <TopicListItem
+      key={topic.id}
+      slug={topic.slug} 
+      title={topic.title}  />)
+  });
   return (
-    <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
-    </div>
+    <section className="top-nav-bar__topic-list">
+      {topics}
+    </section>
   );
 };
 
