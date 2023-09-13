833986127b5ab39c988784ef757272ba99de31c2
Prestin Liu <liuprestin@proton.me>
2023-09-12 22:55:32 -0700

    a single photo item with additional information, props display information as intended

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-
+import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  return (
+    <article className="PhotoListItem photo-list__item" id={props.id}>
+      <PhotoFavButton></PhotoFavButton>
+      <img className="photo-list__image" src={props.urls.regular}></img>
+      
+      <div className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={props.user.profile}
+        ></img>
+        <div className="">
+          <p className="photo-list__user-info">{props.user.name}</p>
+          <p className="photo-list__user-info">{props.user.username}</p>
+          <p className="photo-list__user-location">
+            {props.location.city}, {props.location.country}
+          </p>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default PhotoListItem;
