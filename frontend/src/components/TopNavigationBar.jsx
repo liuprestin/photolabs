eb6a2f4c7f2dcc8755ecf4615f0b6a8a0a2fced5
Prestin Liu <liuprestin@proton.me>
2023-09-12 23:00:19 -0700

    Navbar component for webpage navigation

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import TopicList from './TopicList';
+import FavBadge from './FavBadge';
+
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+
+
+const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList topicListData={props.topicListData}/>
+      <FavBadge/>
     </div>
   )
 }
