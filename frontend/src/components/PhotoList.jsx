bb4eb9aed40b1f56bb0f206d7caa8af1167e2dc8
Prestin Liu <liuprestin@proton.me>
2023-09-17 12:54:47 -0700

    Adding state tracking for all photo components to toggle the notification in the Navbar or not

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
-
 const PhotoList = (props) => {
-  const photos = props.photoListData.map(photo => {
+  const [favPhotoSet, toggleFavPhotoSet] = useState(new Set());
+
+  const onPhotoFavorited = (photoId, toggleFavorite) => {
+    toggleFavPhotoSet((prevState) => {
+      const newFavPhotos = new Set(prevState);
+      if (toggleFavorite) {
+        newFavPhotos.add(photoId);
+      } else {
+        newFavPhotos.delete(photoId);
+      }
+      props.setDoesFavPhotoExist(newFavPhotos.size > 0);
+
+      return newFavPhotos;
+    });
+};
+  const photos = props.photoListData.map((photo) => {
     return (
-    <PhotoListItem
-      key={photo.id}
-      location={photo.location} 
-      urls={photo.urls} 
-      user={photo.user}  />)
+      <PhotoListItem
+        key={photo.id}
+        location={photo.location}
+        urls={photo.urls}
+        user={photo.user}
+        onPhotoFavorited={(isFavorited) =>
+          onPhotoFavorited(photo.id, isFavorited)
+        }
+      />
+    );
   });
-  return (
-    <section className="photo-list">
-      {photos}
-    </section>
-  );
+  return <section className="photo-list">{photos}</section>;
 };
 
 export default PhotoList;
