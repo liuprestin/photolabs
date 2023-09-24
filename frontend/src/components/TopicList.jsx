de2709fbac40f6e62c682eda59f6235586ace3d1
Prestin Liu <liuprestin@proton.me>
2023-09-23 21:15:38 -0700

    document format prettification

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -4,18 +4,12 @@ import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
 const TopicList = (props) => {
-  const topics = props.topicListData.map(topic => {
+  const topics = props.topicListData.map((topic) => {
     return (
-    <TopicListItem
-      key={topic.id}
-      slug={topic.slug} 
-      title={topic.title}  />)
+      <TopicListItem key={topic.id} slug={topic.slug} title={topic.title} />
+    );
   });
-  return (
-    <section className="top-nav-bar__topic-list">
-      {topics}
-    </section>
-  );
+  return <section className="top-nav-bar__topic-list">{topics}</section>;
 };
 
 export default TopicList;
