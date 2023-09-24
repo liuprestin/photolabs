ca3afedf3f9c98e964a592a21ff94d2eace8c1de
Prestin Liu <liuprestin@proton.me>
2023-09-23 21:16:15 -0700

    document format prettification

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -3,11 +3,7 @@ import React from "react";
 import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
-  return (
-    <div className="topic-list__item">
-      {props.slug}
-    </div>
-  );
+  return <div className="topic-list__item">{props.slug}</div>;
 };
 
 export default TopicListItem;
